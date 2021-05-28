8893006de860e95d67dbe0aae50721c1aaf5cf59
DuongHuy1111 <huy.alex.1111@gmail.com>
2021-05-28 14:06:11 +0700

    Count Down Thread

---
+++ src/com/company/CountDownThread.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CountDownThread extends Thread {
+    @Override
+    public void run() {
+        int count = 10;
+        for (int i = count; i > 0; i--) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Time over");
+    }
+}
